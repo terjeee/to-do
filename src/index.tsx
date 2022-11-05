09cf40483086d541e8792ee3c4fca5011a171bb2
terjeee <96795863+terjeee@users.noreply.github.com>
2022-11-05 23:37:27 +0100

    css

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import { createRoot } from "react-dom/client";
 
 import App from "./App";
 
-import "./index.css";
+import "./styles/index.scss";
 
 const container = document.getElementById("root")!;
 const root = createRoot(container);
