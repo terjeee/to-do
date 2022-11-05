26b3edf542a996051bc7b43e8414e242c2eceb55
terjeee <96795863+terjeee@users.noreply.github.com>
2022-11-05 01:31:53 +0100

    + IconPlus for ToDoAdd

---
+++ src/assets/IconPlus.tsx
@@ -0,0 +1,24 @@
+const IconPlus = (props: { style?: {} }) => (
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 50 50" xmlSpace="preserve" {...props}>
+    <circle
+      style={{
+        fill: "#064789",
+      }}
+      cx={25}
+      cy={25}
+      r={25}
+    />
+    <path
+      style={{
+        fill: "none",
+        stroke: "#fff",
+        strokeWidth: 2,
+        strokeLinecap: "round",
+        strokeLinejoin: "round",
+        strokeMiterlimit: 10,
+      }}
+      d="M25 13v25M37.5 25h-25"
+    />
+  </svg>
+);
+export default IconPlus;
