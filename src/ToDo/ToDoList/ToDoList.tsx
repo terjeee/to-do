f2e8583d44b5f55055e7abc33ec84f1b15097567
terjeee <96795863+terjeee@users.noreply.github.com>
2022-11-07 06:13:19 +0100

    render list + onClick={TODO_DELETE}

--- src/ToDo/ToDoList/ToDoList.tsx
+++ src/ToDo/ToDoList/ToDoList.tsx
@@ -1,11 +1,28 @@
+import { useDispatch } from "react-redux";
+import { TODO_REMOVE } from "../../store/slices/sliceToDo";
+
 import styles from "./ToDoList.module.scss";
 
-export default function ToDoList() {
+interface Props {
+  list: { id: string; todo: string }[];
+}
+
+export default function ToDoList(props: Props) {
+  const reduxDispatch = useDispatch();
+
+  const handleDeleteToDo = (event: React.MouseEvent<HTMLElement>) => {
+    const targetId = event.currentTarget.id;
+    console.log(targetId);
+    reduxDispatch(TODO_REMOVE({ id: targetId }));
+  };
+
   return (
     <ul className={styles.list}>
-      <li>ITEM</li>
-      <li>ITEM</li>
-      <li>ITEM</li>
+      {props.list.map((todo) => (
+        <li key={todo.id} id={todo.id} onClick={handleDeleteToDo}>
+          {todo.todo}
+        </li>
+      ))}
     </ul>
   );
 }
