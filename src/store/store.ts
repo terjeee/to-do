a6284b6e59354a560813426b4964c42ce1e0b7f7
terjeee <96795863+terjeee@users.noreply.github.com>
2022-11-07 04:04:15 +0100

    aner ikke hva jeg gjør, men det funker

--- src/store/store.ts
+++ src/store/store.ts
@@ -2,11 +2,6 @@ import { configureStore } from "@reduxjs/toolkit";
 
 import sliceToDo from "./slices/sliceToDo";
 
-const store = configureStore({ reducer: { to: sliceToDo } });
-
-// // Infer the `RootState` and `AppDispatch` types from the store itself
-export type RootState = ReturnType<typeof store.getState>;
-// // Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
-export type AppDispatch = typeof store.dispatch;
+const store = configureStore({ reducer: { todo: sliceToDo.reducer } });
 
 export default store;
