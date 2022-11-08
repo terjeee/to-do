145a4165dd7ccf68c6169de5ba9e3cc8e9475759
terjeee <96795863+terjeee@users.noreply.github.com>
2022-11-09 00:59:56 +0100

    :)

--- src/ToDo/ToDo.tsx
+++ src/ToDo/ToDo.tsx
@@ -5,19 +5,14 @@ import ToDoAdd from "./ToDoAdd/ToDoAdd";
 import ToDoList from "./ToDoList/ToDoList";
 
 import styles from "./ToDo.module.scss";
-import Date from "./Date/Date";
 
 export default function ToDo() {
   const toDoState = useSelector(stateToDo);
-
   console.log(toDoState);
 
   return (
     <section className={styles.todo}>
-      <header>
-        <Date />
-        <ToDoAdd />
-      </header>
+      <ToDoAdd />
       <ToDoList list={toDoState} />
     </section>
   );
