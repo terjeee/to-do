214876ed59f2ab87fb944500cfa96d49055e114d
terjeee <96795863+terjeee@users.noreply.github.com>
2022-11-09 00:52:48 +0100

    + toggle_active på todo-item

--- src/store/slices/sliceToDo.ts
+++ src/store/slices/sliceToDo.ts
@@ -4,6 +4,7 @@ import store from "../store";
 interface State {
   id: string;
   todo: string;
+  active: boolean;
 }
 
 const initialState: State[] = [];
@@ -13,8 +14,16 @@ const sliceToDo = createSlice({
   initialState,
   reducers: {
     TODO_ADD: (state, action: PayloadAction<{ todo: string }>) => {
-      state.push({ id: String(Date.now()), todo: action.payload.todo });
+      state.push({ id: String(Date.now()), todo: action.payload.todo, active: true });
     },
+    TODO_TOGGLE_INACTIVE: (state, action: PayloadAction<{ id: string }>) =>
+      state.map((todo) => {
+        if (todo.id === action.payload.id) {
+          return { ...todo, active: !todo.active };
+        }
+
+        return todo;
+      }),
     TODO_REMOVE: (state, action: PayloadAction<{ id: string }>) => {
       return state.filter((todo) => todo.id !== action.payload.id);
     },
@@ -26,7 +35,7 @@ type RootState = ReturnType<typeof store.getState>;
 export const stateToDo = (state: RootState) => state.todo;
 
 // export actionToDo
-export const { TODO_ADD, TODO_REMOVE } = sliceToDo.actions;
+export const { TODO_ADD, TODO_TOGGLE_INACTIVE, TODO_REMOVE } = sliceToDo.actions;
 
 // export til store.ts
 export default sliceToDo;
