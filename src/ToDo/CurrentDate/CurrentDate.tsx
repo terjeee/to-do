78ddecbb51d24247012fc5cc63da88ca6cf8e3ef
terjeee <96795863+terjeee@users.noreply.github.com>
2022-11-09 00:59:06 +0100

    + CurrentDate

--- src/ToDo/CurrentDate/CurrentDate.tsx
+++ src/ToDo/CurrentDate/CurrentDate.tsx
@@ -1,15 +1,9 @@
 import styles from "./CurrentDate.module.scss";
 
-const monthPairing = ["Januar", "Februar", "Mars", "April", "Mai", 'Juni', 'Juli', 'August', 'September', 'Oktober', 'November', 'Desember']; /* prettier-ignore */
-
 function CurrentDate() {
-  const today = new Date();
-  const date = today.getDate();
-  const month = monthPairing[today.getMonth()];
+  const date = new Date().toLocaleString("nb-NO", { day: "numeric", month: "long" });
 
-  const displayDate = `${date}. ${month}`;
-
-  return <h2 className={styles.time}>{displayDate}</h2>;
+  return <h2 className={styles.date}>{date}</h2>;
 }
 
 export default CurrentDate;
