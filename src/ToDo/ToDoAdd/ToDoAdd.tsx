a6013e342a122d9b5ec07a530c6dbf00a98b5cdb
terjeee <96795863+terjeee@users.noreply.github.com>
2022-11-09 01:04:30 +0100

    :)

--- src/ToDo/ToDoAdd/ToDoAdd.tsx
+++ src/ToDo/ToDoAdd/ToDoAdd.tsx
@@ -1,39 +1,44 @@
 import React, { useState, useRef } from "react";
+
 import { useDispatch } from "react-redux";
 import { TODO_ADD } from "../../store/slices/sliceToDo";
 
-import IconPlus from "../../assets/IconPlus";
-import Modal from "../../components/Modal";
+import CurrentDate from "../CurrentDate/CurrentDate";
+import { IconPlus, IconMinus } from "../../assets/index";
 
 import styles from "./ToDoAdd.module.scss";
 
 export default function ToDoHeader() {
   const refInput = useRef<HTMLInputElement | null>(null);
-  const [showModal, setShowModal] = useState(false);
+  const [showInput, setShowInput] = useState(false);
   const dispatch = useDispatch();
 
   const toggleModal = () => {
-    setShowModal((state) => !state);
+    setShowInput((state) => !state);
   };
 
   const handleSubmitToDo = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     const newToDo = String(refInput.current?.value);
     dispatch(TODO_ADD({ todo: newToDo }));
-    setShowModal((state) => !state);
   };
 
   return (
     <>
-      <button className={styles.btn} onClick={toggleModal}>
-        <IconPlus />
-      </button>
-      <Modal show={showModal} toggleModal={toggleModal}>
-        <form className={styles.modal} onSubmit={handleSubmitToDo}>
+      <div className={styles.header}>
+        <div className={styles.title}>
+          <CurrentDate />
+          <h1>TODO</h1>
+        </div>
+        <button className={styles.btn} onClick={toggleModal}>
+          {!showInput ? <IconPlus /> : <IconMinus />}
+        </button>
+      </div>
+      {showInput && (
+        <form className={styles.inputField} onSubmit={handleSubmitToDo}>
           <input ref={refInput} autoFocus />
-          <button>Add</button>
         </form>
-      </Modal>
+      )}
     </>
   );
 }
