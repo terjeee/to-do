9c937ee107dce6d5339748fc2db8a00ca9f33dda
terjeee <96795863+terjeee@users.noreply.github.com>
2022-11-09 01:06:27 +0100

    (fiks output-- gjør state om til todoList)

--- src/ToDo/ToDoList/ToDoList.tsx
+++ src/ToDo/ToDoList/ToDoList.tsx
@@ -1,28 +1,48 @@
-import { useDispatch } from "react-redux";
-import { TODO_REMOVE } from "../../store/slices/sliceToDo";
+import { useState } from "react";
+
+import ToDoListItem from "./components/ToDoListItem";
 
 import styles from "./ToDoList.module.scss";
 
 interface Props {
-  list: { id: string; todo: string }[];
+  list: { id: string; todo: string; active: boolean }[];
 }
 
 export default function ToDoList(props: Props) {
-  const reduxDispatch = useDispatch();
-
-  const handleDeleteToDo = (event: React.MouseEvent<HTMLElement>) => {
-    const targetId = event.currentTarget.id;
-    console.log(targetId);
-    reduxDispatch(TODO_REMOVE({ id: targetId }));
-  };
+  const [displayList, setDisplayList] = useState("all");
 
   return (
-    <ul className={styles.list}>
-      {props.list.map((todo) => (
-        <li key={todo.id} id={todo.id} onClick={handleDeleteToDo}>
-          {todo.todo}
-        </li>
-      ))}
-    </ul>
+    <div>
+      {displayList === "all" && (
+        <ul className={styles.list}>
+          {props.list.map((todo) => (
+            <ToDoListItem key={todo.id} id={todo.id} todo={todo.todo} />
+          ))}
+        </ul>
+      )}
+      {displayList === "active" && (
+        <ul>
+          {props.list
+            .filter((todo) => todo.active)
+            .map((todo) => (
+              <ToDoListItem key={todo.id} id={todo.id} todo={todo.todo} />
+            ))}
+        </ul>
+      )}
+      {displayList === "completed" && (
+        <ul>
+          {props.list
+            .filter((todo) => !todo.active)
+            .map((todo) => (
+              <ToDoListItem key={todo.id} id={todo.id} todo={todo.todo} />
+            ))}
+        </ul>
+      )}
+      <div>
+        <button onClick={() => setDisplayList("all")}>All</button>
+        <button onClick={() => setDisplayList("active")}>Active</button>
+        <button onClick={() => setDisplayList("completed")}>Completed</button>
+      </div>
+    </div>
   );
 }
