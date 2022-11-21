78b48186c70a26795e19d87a8e120d5a304edca4
terjeee <96795863+terjeee@users.noreply.github.com>
2022-11-21 01:40:43 +0100

    -icon

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,6 +1,5 @@
 export { default as IconAdd } from "./IconAdd";
 export { default as IconDelete1 } from "./IconDelete1";
 export { default as IconDelete2 } from "./IconDelete2";
-export { default as IconDelete3 } from "./IconDelete3";
 export { default as IconMinus } from "./IconMinus";
 export { default as IconPlus } from "./IconPlus";
